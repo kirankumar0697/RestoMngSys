d96c933e6fd8db3d8d5b09e69f8adf0c3df16844
kirankumar <kirannarare@gmail.com>
2020-05-31 09:20:58 +0530

    empty project

---
+++ RestoMngSys/src/main/java/com/resto/RestoMngSys/RestoMngSysApplication.java
@@ -0,0 +1,13 @@
+package com.resto.RestoMngSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoMngSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoMngSysApplication.class, args);
+	}
+
+}
---
+++ RestoMngSys/src/test/java/com/resto/RestoMngSys/RestoMngSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resto.RestoMngSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoMngSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
